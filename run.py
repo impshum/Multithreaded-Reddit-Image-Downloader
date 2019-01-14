454744d0654f7750981c8763960b34e6b4a988d7
impshum <impshum@sausage.local>
2019-01-14 01:37:13 +0000

    Update run.py

--- run.py
+++ run.py
@@ -2,48 +2,66 @@ import praw
 import threading
 from requests import get
 from multiprocessing.pool import ThreadPool
+import os
 
 
 client_id = 'XXXX'
 client_secret = 'XXXX'
 user_agent = 'Multithreaded Reddit image downloader thing (by /u/impshum)'
+image_directory = 'zzz'
+thread_count = 16
 
-target_subreddit = input('which subreddit?: ')
-image_count = input('how many images?: ')
-order = input('hot/new/top?: ')
+target_subreddit = input('Which subreddit?: ')
+image_count = input('How many images?: ')
+order = input('Hot/New/Top?: ')
+
+order = order.lower()
 
 reddit = praw.Reddit(client_id=client_id,
-                     client_secret=client_secret,
-                     user_agent=user_agent)
+                     client_secret=client_secret, user_agent=user_agent)
+
+
+def get_order():
+    if order == 'hot':
+        ready = reddit.subreddit(target_subreddit).hot(limit=None)
+    elif order == 'top':
+        ready = reddit.subreddit(target_subreddit).top(limit=None)
+    elif order == 'new':
+        ready = reddit.subreddit(target_subreddit).new(limit=None)
+    return ready
 
 
 def get_img(what):
-    image = 'zzz/{}'.format(what.split('/')[-1])
-    img = get(url).content
+    image = '{}/{}/{}'.format(image_directory,
+                              target_subreddit, what.split('/')[-1])
+    img = get(what).content
     with open(image, 'wb') as f:
         f.write(img)
 
 
-order = order.lower()
+def make_dir():
+    directory = f'{image_directory}/{target_subreddit}'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-if order == 'hot':
-    ready = reddit.subreddit('pics').hot(limit=None)
-elif order == 'top':
-    ready = reddit.subreddit('pics').top(limit=None)
-elif order == 'new':
-    ready = reddit.subreddit('pics').new(limit=None)
 
-c = 1
-images = []
+def main():
+    c = 1
+    images = []
+    make_dir()
+    for submission in get_order():
+        url = submission.url
+        if url.endswith(('.jpg', '.png', '.gif', '.jpeg')):
+            images.append(url)
+            c += 1
+            if int(image_count) < c:
+                break
 
-for submission in ready:
-    url = submission.url
-    if url.endswith(('.jpg', '.png', '.gif', '.jpeg')):
-        images.append(url)
-        c += 1
-        if int(image_count) < c:
-            break
+    results = ThreadPool(thread_count).imap_unordered(get_img, images)
+    for path in results:
+        pass
 
-results = ThreadPool(8).imap_unordered(get_img, images)
-for path in results:
-    print('DONE')
+    print('Done')
+
+
+main()
